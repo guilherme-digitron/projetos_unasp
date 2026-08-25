3c9b51e4ee330513f7ec018e2925c3d44b76e69c
Guilherme da Silva Cordeiro <guilherme11.amaral2006@gmail.com>
2026-08-25 11:48:11 -0300

    Adicionar configuração Gradle na raiz do projeto para CI/CD

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "projetos_unasp"
+include(":contador-android")
+project(":contador-android").projectDir = File("contador-android")
